6ecb2a5891ed88249077a66f76dadfb629420916
AshishNith <ranjan.ashish9992@gmail.com>
2025-02-23 19:19:52 +0530

    Gallary Updated and Scrollbar color fixed

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -5,12 +5,11 @@ const Gallery = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
 
-  // Using placeholder images instead of external URLs
   const images = [
-      'https://res.cloudinary.com/dosnuagvu/image/upload/v1739553511/Images_in_about_us_p1_b9avno.jpg',
-      'https://res.cloudinary.com/dosnuagvu/image/upload/v1739553515/Images_in_about_us_p2_jsvalz.jpg',
-      'https://res.cloudinary.com/dosnuagvu/image/upload/v1739553519/Images_in_about_us_p4_oh1yqx.jpg',
-      'https://res.cloudinary.com/dosnuagvu/image/upload/v1739555533/Images_in_about_us_p3-min_bq9ywj.jpg'
+    'https://res.cloudinary.com/dosnuagvu/image/upload/v1739553511/Images_in_about_us_p1_b9avno.jpg',
+    'https://res.cloudinary.com/dosnuagvu/image/upload/v1739553515/Images_in_about_us_p2_jsvalz.jpg',
+    'https://res.cloudinary.com/dosnuagvu/image/upload/v1739553519/Images_in_about_us_p4_oh1yqx.jpg',
+    'https://res.cloudinary.com/dosnuagvu/image/upload/v1739555533/Images_in_about_us_p3-min_bq9ywj.jpg'
   ];
 
   const scrollGallery = useCallback((direction) => {
@@ -23,21 +22,12 @@ const Gallery = () => {
     });
   }, [images.length]);
 
-  // Auto-scroll functionality
   useEffect(() => {
     let intervalId;
-    
     if (!isPaused) {
-      intervalId = setInterval(() => {
-        scrollGallery('right');
-      }, 2000); 
+      intervalId = setInterval(() => scrollGallery('right'), 2000);
     }
-
-    return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-    };
+    return () => intervalId && clearInterval(intervalId);
   }, [isPaused, scrollGallery]);
 
   return (
@@ -46,32 +36,24 @@ const Gallery = () => {
       onMouseEnter={() => setIsPaused(true)}
       onMouseLeave={() => setIsPaused(false)}
     >
-      {/* Navigation Buttons */}
       <button
-        className="absolute left-4 top-1/2 transform -translate-x-1/2 bg-black/60 p-3 rounded-full hover:bg-pink-400/70 transition-all z-30"
+        className="absolute left-4 top-1/2 -translate-x-1/2 bg-black/60 p-3 rounded-full hover:bg-pink-400/70 transition-all z-30"
         onClick={() => scrollGallery('left')}
       >
         <ChevronLeft className="text-white w-6 h-6" />
       </button>
       
       <button
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-black/60 p-3 rounded-full hover:bg-pink-400/70 transition-all z-30"
+        className="absolute right-4 top-1/2 -translate-y-1/2 bg-black/60 p-3 rounded-full hover:bg-pink-400/70 transition-all z-30"
         onClick={() => scrollGallery('right')}
       >
         <ChevronRight className="text-white w-6 h-6" />
       </button>
 
-      {/* Image Container */}
-      <div className="relative h-full w-full">
+      <div className="flex h-full transition-transform duration-500 ease-in-out"
+           style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
         {images.map((src, index) => (
-          <div
-            key={index}
-            className={`absolute top-0 left-0 h-full w-full transition-transform duration-500 ease-in-out ${currentIndex === index ? 'rotate-y-0' : 'rotate-y-180'}`}
-            style={{
-              zIndex: currentIndex === index ? 10 : 0,
-              transform: currentIndex === index ? 'rotateY(0deg)' : 'rotateY(180deg)',
-            }}
-          >
+          <div key={index} className="w-full h-full flex-shrink-0">
             <img
               src={src}
               alt={`Gallery ${index + 1}`}
@@ -81,8 +63,7 @@ const Gallery = () => {
         ))}
       </div>
 
-      {/* Navigation Dots */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2 z-30">
+      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex space-x-2 z-30">
         {images.map((_, index) => (
           <button
             key={index}
